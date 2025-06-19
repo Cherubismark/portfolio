d59d6ff813e12de621e6a59d7b7acd58f1755069
Cherubismark <bismark.ckoskei@gmail.com>
2025-06-20 01:25:59 +0300

    Adding projects to the portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const nameHeading = document.querySelector("h1");
+  nameHeading.addEventListener("click", () => {
+    alert("Welcome to Bismark's Portfolio!");
+  });
+});
